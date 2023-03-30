ae328c0c6cc86f396960e2190b62ede6c0943fb6
Flylink <flylink92@mail.ru>
2023-03-31 00:37:20 +0700

    webserver logic and page

---
+++ src/main/java/org/example/MainController.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("loginDate", LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        return "greeting";
+    }
+}
